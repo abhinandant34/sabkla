f350803984ab42da6b6cd0a1eb3261cded4b4058
Abhinandan Thakur <73791479+201537-aadi@users.noreply.github.com>
2021-05-24 20:10:23 +0530

    css fixes

--- src/Slideshow.js
+++ src/Slideshow.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css'
-import './Crause.css'
+import './Slideshow.css'
 
 
 const slideImages = [
